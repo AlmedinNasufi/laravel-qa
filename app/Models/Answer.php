1e120039ec337bec3ad60a4d3881e14534496d05
AlmedinNasufi <almedin.nasufi20@gmail.com>
2021-05-02 15:54:11 +0200

    displays answers and question

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -33,4 +33,8 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
     }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
