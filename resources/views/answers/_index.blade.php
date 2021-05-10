a77a707230ada85b9d85ba9f92ab235045e4b63c
AlmedinNasufi <almedin.nasufi20@gmail.com>
2021-05-10 14:58:25 +0200

    Display all questions in root, sort all the answers, hide the answer's container with no answer, and show an alert when tthere is no question on page

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,54 +1,56 @@
-<div class="row mt-4">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-title">
-                    <h2> {{ $answersCount. " " . Str::plural('Answer',$question->answers_count) }}</h2>
-                </div>
-                <hr>
-                @include('layouts._message')
-
-                @foreach($answers as $answer)
-                    <div class="media">
-                        @include('shared._vote',[
-                            'model' => $answer
-                            ])
-                        <div class="media-body">
-                            {!! $answer->body_html !!}
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="ml-auto">
-                                        {{--                                            @if(\Illuminate\Support\Facades\Auth::user()->can('update-question',$question))--}}
-                                        @can('update',$answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        {{--                                            @endif--}}
-                                        {{--                                            @if(\Illuminate\Support\Facades\Auth::user()->can('delete-question',$question))--}}
-                                        @can('delete', $answer)
-                                            <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick=" return confirm('Are you sure')" >Delete</button>
-                                            </form>
-                                            {{--                                            @endif--}}
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    @include('shared._author',[
-                                    'model' => $answer,
-                                    'label' => 'Answered'
-                                    ])
-                                </div>
-                            </div>
-
-                        </div>
+@if($answersCount > 0)
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2> {{ $answersCount. " " . Str::plural('Answer',$question->answers_count) }}</h2>
                     </div>
                     <hr>
-                @endforeach
+                    @include('layouts._message')
 
+                    @foreach($answers as $answer)
+                        <div class="media">
+                            @include('shared._vote',[
+                                'model' => $answer
+                                ])
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="ml-auto">
+                                            {{--                                            @if(\Illuminate\Support\Facades\Auth::user()->can('update-question',$question))--}}
+                                            @can('update',$answer)
+                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endcan
+                                            {{--                                            @endif--}}
+                                            {{--                                            @if(\Illuminate\Support\Facades\Auth::user()->can('delete-question',$question))--}}
+                                            @can('delete', $answer)
+                                                <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick=" return confirm('Are you sure')" >Delete</button>
+                                                </form>
+                                                {{--                                            @endif--}}
+                                            @endcan
+                                        </div>
+                                    </div>
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include('shared._author',[
+                                        'model' => $answer,
+                                        'label' => 'Answered'
+                                        ])
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
