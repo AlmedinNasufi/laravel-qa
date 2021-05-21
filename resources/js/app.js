d0298ce77f4d3efa2c270dc64e03dd915498b4e8
AlmedinNasufi <almedin.nasufi20@gmail.com>
2021-05-21 19:32:26 +0200

    Create Answers component and implement vue list rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,10 +13,20 @@ Vue.use(Authorization);
 
 
 Vue.component('user-info', require('./components/UserInfo.vue').default);
-Vue.component('answer', require('./components/Answer.vue').default);
-Vue.component('favorite',require('./components/Favorite.vue').default);
-Vue.component('accept',require('./components/Accept.vue').default);
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//We don`t need to declare a vue component for answer because we imported this component as child of Answers component
+//Vue.component('answer', require('./components/Answer.vue').default);
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+Vue.component('answers', require('./components/Answers.vue').default);
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//We don`t need to declare a vue component for favorite and accepts because we imported them in vote component
+// Vue.component('favorite',require('./components/Favorite.vue').default);
+// Vue.component('accept',require('./components/Accept.vue').default);
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 Vue.component('vote',require('./components/Vote.vue').default);
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
